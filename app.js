1ec2bf4c6f43f0250eb5e70638af81688e572cea
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2021-08-25 16:50:04 -0700

    Created html, css, and js for Animal Farm

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
 // import functions
+const catImg = document.getElementById('cat');
+const catSound = document.getElementById('audiocat');
+const catCounter = document.getElementById('count')
+
+const dogImg = document.getElementById('dog');
+const dogSound = document.getElementById('audiodog');
+
+const horseImg = document.getElementById('horse');
+const horseSound = document.getElementById('audiohorse');
+
+let catClicks = 0;
 
 // reference needed DOM elements
-
+ 
 
 // set event listeners 
+
+catImg.addEventListener('click', () => {
+  catSound.play();
+  catClicks ++;
+  catCounter.textContent = 'You have clicked the cat ' + catClicks + ' times!';
+});
+
+dogImg.addEventListener('click', () => {
+  dogSound.play();
+});
+
+horseImg.addEventListener('click', () => {
+  horseSound.play();
+});
+
   // get user input(s)
   // do any needed work with the value(s)
   // update DOM to reflect new value(s)
